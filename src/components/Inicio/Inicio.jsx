b98754a8c58d48b554d06794c2357f4223abc76d
Wisllensr <wisllensr@gmail.com>
2026-02-06 19:39:36 -0300

    Finalização pagina perfil

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,9 +1,18 @@
+import styles from "./Inicio.module.scss"
+import ImgInicial from "../../assets/img-tela-inicial.png"
+
 function Inicio() {
   return (
-    <>
-    
-    <h2> Inicio </h2>
-    </>
+    <section className={styles.containerTelaInicial}>
+
+      <div>
+    <h1> Projetos Sociais que transformam</h1>
+    <p>Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade.</p>
+      </div>
+
+      <img src={ImgInicial} alt="Imagem da tela inicial" />
+
+    </section>
   )
 }
 export default Inicio
--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,9 +1,40 @@
+import styles from "./Perfil.module.scss"
+import ImgPerfil from "../../assets/FotoPerfil.jpeg"
+
 function Perfil() {
   return (
-    <>
-    
-    <h2> perfil </h2>
-    </>
+    <section className={styles.containerTelaPerfil}>
+
+      <article>
+
+        <img src={ImgPerfil} alt="Foto de perfil" />
+
+        <div className={styles.cardInfo}>
+          <h2>Wisllen Ramos</h2>
+          <h3>Voluntário Ativo</h3>
+          <p className={styles.paragrafoSobreMim}>Apaixonado por fazer a diferença na comunidade. Acredito que pequenas ações podem transformar vidas e estou sempre em busca de novas oportunidades para ajudar.</p>
+
+          <address>
+            <p>Rio de Janeiro, RJ</p>
+            <p>wisllensr@gamil.com</p>
+            <p>membro desde 2025</p>
+          </address>
+
+          <ul className={styles.listaInteresses}>
+            <li>Educação</li>
+            <li>Meio Ambiente</li>
+            <li>Assistência Social</li>
+            <li>Design</li>
+          </ul>
+
+
+        </div>
+
+
+
+      </article>
+
+    </section>
   )
 }
 export default Perfil
